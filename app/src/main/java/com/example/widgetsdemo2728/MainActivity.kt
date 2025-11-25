52c4a633bc83e86a9944eeb3aabae30c8d14b510
Ivan Petrushin <ivan.kiel@gmail.com>
2025-11-25 10:51:01 +0800

    CheckBox added

--- app/src/main/java/com/example/widgetsdemo2728/MainActivity.kt
+++ app/src/main/java/com/example/widgetsdemo2728/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.widgetsdemo2728
 
 import android.os.Bundle
+import android.widget.CheckBox
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,8 +10,17 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        val checkBox = findViewById<CheckBox>(R.id.testbox)
+        checkBox?.setOnCheckedChangeListener { buttonView, isChecked ->
+            // Handle the checked state change here
+            if (isChecked) {
+                // Checkbox is checked
+            } else {
+                // Checkbox is unchecked
+            }
+        }
 
     }
 }
